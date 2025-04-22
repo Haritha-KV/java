68801593ff2f9e3e7fc3c19a9291a38ab52aef84
Haritha-KV <harithavijay114@gmail.com>
2025-04-22 13:57:20 +0530

    Add files via upload

---
+++ Synchronization/BankAccount.java
@@ -0,0 +1,17 @@
+package com.program;
+//BankAccount.java
+class BankAccount {
+ private int balance = 0;
+
+ // Synchronized method to prevent race condition
+ public synchronized void deposit(int amount) {
+     int newBalance = balance + amount;
+     // Simulate delay to cause race condition if not synchronized
+     try { Thread.sleep(100); } catch (InterruptedException e) {}
+     balance = newBalance;
+ }
+
+ public int getBalance() {
+     return balance;
+ }
+}
